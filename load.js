776e519e4df88c8e763f3a16d373f582a9c52707
Guy Royse <guy@guyroyse.com>
2021-02-18 16:36:20 -0500

    the code I literally just record myself writing

---
+++ load.js
@@ -0,0 +1,27 @@
+import csv from 'csv-parser'
+import Redis from 'ioredis'
+
+import fs from 'fs'
+
+let r = new Redis()
+let p = r.pipeline()
+
+fs.createReadStream('data/bfro_reports_geocoded.csv')
+  .pipe(csv())
+  .on('data', data => {
+    let { number, title, date, observed, classification, county, state, latitude, longitude, location_details } = data
+
+    let id = parseInt(number)
+    title = title.replace(/^Report \d*: /, '')
+    county = county.replace(/ County$/, '')
+    let location = (longitude && latitude) ? `${longitude},${latitude}` : ''
+
+    let key = `sighting:${id}`
+    let values = { id, title, date, observed, classification, county, state, location, location_details }
+
+    r.hset(key, values)
+  })
+  .on('end', () => {
+    p.exec()
+    r.disconnect()
+  })
