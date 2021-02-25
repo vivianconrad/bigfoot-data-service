437716acc0fa7636b9512d35127b95a8d2350a9f
Guy Royse <guy@guyroyse.com>
2021-02-25 17:35:54 -0500

    code from second video

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import Redis from 'ioredis'
+
+import config from './config.js'
+import SightingData from './sightings-data.js'
+
+let app = new express()
+app.use(express.json())
+
+let sightingData = new SightingData()
+sightingData.init()
+
+app.get('/', (req, res) => {
+  res.send({
+    name: config.SERVICE_NAME,
+    version: config.SERVICE_VERSION
+  })
+})
+
+app.get('/sighting/:id', async (req, res) => {
+  res.send(await sightingData.findById(req.params.id))
+})
+
+app.get('/sightings/state/:state', async (req, res) => {
+  res.send(await sightingData.findByState(req.params.state))
+})
+
+app.get('/sightings/county/:county/state/:state', async (req, res) => {
+  res.send(await sightingData.findByCountyState(req.params.county, req.params.state))
+})
+
+app.get('/sightings/containing/:text', async (req, res) => {
+  res.send(await sightingData.findContaining(req.params.text))
+})
+
+app.get('/sightings/state/:state/containing/:text', async (req, res) => {
+  res.send(await sightingData.findByStateContaining(req.params.state, req.params.text))
+})
+
+app.get('/sightings/near/lat/:latitude/long/:longitude/within/:radius/mi', async (req, res) => {
+  res.send(await sightingData.findNear(req.params.latitude, req.params.longitude, req.params.radius, 'mi'))
+})
+
+app.get('/sightings/near/lat/:latitude/long/:longitude/within/:radius/km', async (req, res) => {
+  res.send(await sightingData.findNear(req.params.latitude, req.params.longitude, req.params.radius, 'km'))
+})
+
+app.listen(config.PORT, () => console.log(`Bigfoot is listening on port ${config.PORT}`))
---
+++ sightings-data.js
@@ -0,0 +1,69 @@
+import Redis from 'ioredis'
+import config from './config.js'
+
+const INDEX = 'sightings:index'
+
+class SightingsData {
+  async init() {
+    this.connection = new Redis(config.REDIS_URL)
+
+    let indices = await this.connection.call('FT._LIST')
+
+    if (indices.includes(INDEX)) {
+      await this.connection.call('FT.DROPINDEX', INDEX)
+    }
+
+    await this.connection.call(
+      'FT.CREATE', INDEX,
+      'ON', 'hash',
+      'PREFIX', 1, 'sighting:',
+      'SCHEMA',
+        'title', 'TEXT',
+        'observed', 'TEXT',
+        'location_details', 'TEXT',
+        'location', 'GEO',
+        'county', 'TAG',
+        'state', 'TAG'
+    )
+  }
+
+  async findById(id) {
+    return await this.connection.hgetall(`sighting:${id}`)
+  }
+
+  async findByState(state) {
+    return await this.find(`@state:{${state}}`)
+  }
+
+  async findByCountyState(county, state) {
+    return await this.find(`@county:{${county}} @state:{${state}}`)
+  }
+
+  async findContaining(text) {
+    return await this.find(text)
+  }
+
+  async findByStateContaining(state, text) {
+    return await this.find(`text @state:{${state}}`)
+  }
+
+  async findNear(latitude, longitude, radius, units) {
+    return await this.find(`@location:[${longitude} ${latitude} ${radius} ${units}]`)
+  }
+
+  async find(query) {
+    let [count, ...foundKeysAndSightings] = await this.connection.call('FT.SEARCH', INDEX, query, 'LIMIT', 0, 100)
+    let foundSightings = foundKeysAndSightings.filter((entry, index) => index % 2 !== 0)
+    let sightings = foundSightings.map(sightingArray => {
+      let keys = sightingArray.filter((_, index) => index % 2 === 0)
+      let values = sightingArray.filter((_, index) => index % 2 !== 0)
+      return keys.reduce((sighting, key, index) => {
+        sighting[key] = values[index]
+        return sighting
+      }, {})
+    })
+    return { count, sightings }
+  }
+}
+
+export default SightingsData
